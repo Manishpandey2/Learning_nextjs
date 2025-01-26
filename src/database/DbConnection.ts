957017df086565b95c0d3c993f0630b5129805ab
Manishpande2 <biraalogang@gmail.com>
2025-01-27 00:24:54 +0545

    Add check for existing database connection in DbConnection

--- src/database/DbConnection.ts
+++ src/database/DbConnection.ts
@@ -7,6 +7,11 @@ if(!MONGODB_CS){
 }
 
 const DbConnection = async()=>{
+    
+    if(mongoose.connection.readyState === 1){
+        console.log('Database already connected')
+        return;
+    }
     try{
         await mongoose.connect(MONGODB_CS)
         console.log('Database connected Successfully')
